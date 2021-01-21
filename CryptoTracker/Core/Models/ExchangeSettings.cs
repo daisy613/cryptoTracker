f8b29488a1fdbe2f43aeab3aeaef45c5b9b136f3
Leon Spors <15744333+LeonSpors@users.noreply.github.com>
2021-01-21 20:25:12 +0100

    Add settings, remove example

---
+++ CryptoTracker/Core/Models/ExchangeSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CryptoTracker.Core.Models
+{
+    public class ExchangeSettings
+    {
+        public string Key { get; set; }
+
+        public string Secret { get; set; }
+    }
+}
--- CryptoTracker/Startup.cs
+++ CryptoTracker/Startup.cs
@@ -1,15 +1,9 @@
-using CryptoTracker.Core.Data;
+using CryptoTracker.Core.Services;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CryptoTracker
 {
@@ -21,17 +15,14 @@ namespace CryptoTracker
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<ExchangeDataService>();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -41,7 +32,6 @@ namespace CryptoTracker
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
